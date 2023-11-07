8d7360d4d278270783301179e688a8baaabc326d
DustEyezz <75487794+DustEyezz@users.noreply.github.com>
2023-11-07 18:57:15 +0100

    missing code

--- myscript.js
+++ myscript.js
@@ -1,5 +1,27 @@
 function handleReverseForm(form){
     let word = form.inputbox3.value;
-    document.getElementById("reverseResult").innerHTML = word.split("").reverse().join("");
+    document.getElementById("reverseResult").innerHTML = myReverse(word);
 }
 
+function testFunction(newVar) {
+	return newVar = x + 10;
+}
+
+function myReverse(newVarTest){
+	let result = "";
+	for(let i = newVarTest.length-1; i >= 0; i--){
+		result = result + newVarTest[i];
+	}
+	return result
+}
+
+function testMyType(testedElement){
+	if (typeof(testedElement) == "number"){
+		return "Jestem liczba"
+	} 
+	else {
+		return "Jestem czym innym"
+	}
+}
+
+console.log("5" ==)
