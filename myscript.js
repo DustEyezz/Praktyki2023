b6da4fe2e503adc1678d4326c3169e4b67ee84f9
DustEyezz <75487794+DustEyezz@users.noreply.github.com>
2023-11-14 18:05:00 +0100

    fixed index

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 const genPicture = () => {
-	fetch('https://jsonplaceholder.typicode.com/photos/9999999')
+	fetch('https://jsonplaceholder.typicode.com/photos/1')
 	.then((response) => {
 		if(!response.ok) {
 			throw new Error("Network response was not OK")
